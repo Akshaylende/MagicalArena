3057bfb5afbe8504bf0b566a658f906ed18341dd
Akshay Lende <akshaylende48@gmail.com>
2024-07-26 18:28:29 +0530

    Defining the main method to setup our Game

---
+++ main.py
@@ -0,0 +1,15 @@
+# This will the entry point to run the Game 
+
+# Here we will Initialize our Game and setup
+def main() -> None:
+    print("Welcome to the Magical Arena!")
+    
+    print("Are you ready to take a deep dive into this magical world ?")
+    play = input("press Y for Yes or N for No : ")
+    
+    
+
+    
+# calling the main Function
+if __name__ == "__main__" :
+    main()
